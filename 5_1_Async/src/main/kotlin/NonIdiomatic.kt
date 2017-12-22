e8fafe912219c6548169183700f09998b0b7d07f
Kevin Jones <kevin@rocksolidknowledge.com>
2017-12-22 14:32:30 +0000

    Added chapter 5 demos

---
+++ 5_1_Async/src/main/kotlin/NonIdiomatic.kt
@@ -0,0 +1,23 @@
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+
+// 5. SHow this and explain why non-idiomatic
+// Should opt-in to concurrent code, this is always concurrent
+// use the async builder like in previous demos
+fun main(args: Array<String>) = runBlocking {
+    val result = doWorkAsync("Hello")
+
+    result.join()
+    println(result.getCompleted())
+}
+
+suspend fun doWorkAsync(msg: String): Deferred<Int> = async {
+    log("$msg - Working")
+    delay(500)
+    log("$msg - Work done")
+
+    // return from the closure
+    return@async 42
+}
---
+++ 5_1_Async/src/main/kotlin/UsingDeferred.kt
@@ -0,0 +1,22 @@
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+
+// 6. Show this code, prints out 42
+// 7. Move the println to before the join - this throws an exception
+fun main(args: Array<String>) = runBlocking {
+    val job = launch {
+        println("About to work")
+        val r1 = async { doWorkDeferred("Deferred") }
+
+        r1.join()
+        println("Result is ${r1.getCompleted()}")
+    }
+    job.join()
+}
+ fun doWorkDeferred(msg: String) : Int {
+    log(msg)
+ //   delay(100)
+    return 42
+}
